788b919a16c8fbed8e0c2b69b4ca7c35735406c3
Michael Reel <mykilr@gmail.com>
2021-09-21 00:02:11 +0100

    More or less on par with the unity tutorial part 4.

--- Landmass2D.gd
+++ Landmass2D.gd
@@ -1,20 +1,67 @@
+tool
 extends TextureRect
 
-enum DRAW_MODE { NoiseMap, ColorMap }
-export(DRAW_MODE) var draw_mode = DRAW_MODE.NoiseMap
+const regions = {
+	"Water Deep": Color(0.0, 0.25, 1.0),
+	"Water Shallow": Color(0.0, 0.5, 1.0),
+	"Sand": Color(0.94, 0.9, 0.55),
+	"Grass 1": Color(0.16, 0.66, 0.16),
+	"Grass 2": Color(0.08, 0.42, 0.08),
+	"Rock 1": Color(0.37, 0.24, 0.11),
+	"Rock 2": Color(0.3, 0.2, 0.1),
+	"Snow": Color(0.9, 0.9, 0.9),
+}
 
-export (float) var noise_scale : float = 128.0
-export (int) var octives : int = 4
-export (float, 0.0, 1.0) var persistence : float = 0.5
-export (float) var lacunarity : float = 2.0
-export (Array, Resource) var terrain_types : Array
+enum DRAW_MODE { NoiseMap, ColorMap }
+export(DRAW_MODE) var draw_mode = DRAW_MODE.NoiseMap setget set_draw_mode
+export (float, 1.0, 2048.0) var noise_scale : float = 64.0 setget set_period
+export (int) var octaves : int = 4 setget set_octaves
+export (float, 0.0, 1.0) var persistence : float = 0.5 setget set_persistence
+export (float) var lacunarity : float = 2.0 setget set_lacunarity
+export (Dictionary) var terrain_types : Dictionary = {
+	0.3: regions["Water Deep"],
+	0.4: regions["Water Shallow"],
+	0.45: regions["Sand"],
+	0.55: regions["Grass 1"],
+	0.6: regions["Grass 2"],
+	0.7: regions["Rock 1"],
+	0.9: regions["Rock 2"],
+	1.0: regions["Snow"],
+} setget set_terrain_types
 
 
 func _ready():
+	update_texture_rect()
+
+func set_draw_mode(value):
+	draw_mode = value
+	update_texture_rect()
+
+func set_period(value : float):
+	noise_scale = value
+	update_texture_rect()
+
+func set_octaves(value : int):
+	octaves = value
+	update_texture_rect()
+
+func set_persistence(value : float):
+	persistence = value
+	update_texture_rect()
+
+func set_lacunarity(value : float):
+	lacunarity = value
+	update_texture_rect()
+
+func set_terrain_types(value : Dictionary):
+	terrain_types = value
+	update_texture_rect()
+
+func update_texture_rect():
 	var width : int = rect_size.x
 	var height : int = rect_size.y
 
-	var noise_map = generate_noise_map(width, height, 3, noise_scale, octives, persistence, lacunarity)
+	var noise_map = generate_noise_map(width, height, 3, noise_scale, octaves, persistence, lacunarity)
 	var noise_color_array : PoolByteArray
 	match draw_mode:
 		DRAW_MODE.NoiseMap:
@@ -67,12 +114,13 @@ func generate_region_array(noise_map : Array) -> PoolByteArray:
 	var bytes := PoolByteArray()
 	for row in noise_map:
 		for map_height in row:
-			for region in terrain_types:
-				if map_height <= region.height:
-					bytes.append(region.color.r8)
-					bytes.append(region.color.g8)
-					bytes.append(region.color.b8)
-					bytes.append(region.color.a8)
+			for height in terrain_types:
+				if map_height <= height:
+					var color : Color = terrain_types[height]
+					bytes.append(color.r8)
+					bytes.append(color.g8)
+					bytes.append(color.b8)
+					bytes.append(color.a8)
 					break
 	return bytes
 
@@ -86,3 +134,10 @@ func display_image(noise_image : Image):
 	imageTexture.create_from_image(noise_image)
 	self.texture = imageTexture
 	imageTexture.resource_name = "Land Mass"
+
+func create_region(n : String, h : float, c : Color) -> TerrainType:
+	var region : TerrainType = TerrainType.new()
+	region.terrain_name = n
+	region.height = h
+	region.color = c
+	return region
